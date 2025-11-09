d17f81aebc130b00b4d220a80f63e385cf279cd5
Victor Bessa Marcelino <vitorbessilva@gmail.com>
2025-11-09 12:26:04 -0300

    feat: Atualiza BASE_URL do frontend para o backend no Render

--- NomeDoNovoProjeto/src/api/client.js
+++ NomeDoNovoProjeto/src/api/client.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // Para descobrir o IP no Windows, abra o CMD e digite `ipconfig`.
 // Procure por "Endereço IPv4".
 // -----------------------------------------------------------------
-const BASE_URL = 'http://192.168.0.20:3000'; 
+const BASE_URL = 'https://appagenda-0fat.onrender.com'; 
 
 let authToken = null;
 
